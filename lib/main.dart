013f4462bad8959d6e207e7419c281ee2782c3fa
Wallace <wallace@stallos.com.br>
2026-09-18 21:34:02 -0300

    subindo projeto

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:atv_interfaces/layout_perfil_do_usuario.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      // home: LayoutPerfilDoUsuario(),
+      debugShowCheckedModeBanner: false,)
+  );
+}
